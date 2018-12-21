812beac946e3e32bc8bcbba69db85ca092983738
LordDarthArt <lorddarthart@gmail.com>
2018-12-21 06:30:44 +0300

    import pushover libs added functionality to "send" button going after testing

--- app/src/main/java/tk/lorddarthart/pushovertestapp/SendPushActivity.java
+++ app/src/main/java/tk/lorddarthart/pushovertestapp/SendPushActivity.java
@@ -1,22 +1,69 @@
 package tk.lorddarthart.pushovertestapp;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.support.design.widget.BaseTransientBottomBar;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import static tk.lorddarthart.pushovertestapp.DBPush.savePushToHistory;
+
+import net.pushover.client.PushoverClient;
+import net.pushover.client.PushoverException;
+import net.pushover.client.PushoverMessage;
+import net.pushover.client.PushoverRestClient;
+
 public class SendPushActivity extends AppCompatActivity {
     private CheckBox checkBox;
-    private EditText etMinutes;
+    private Button btnSend;
+    private EditText etDK, etNTitle, etNT, etMinutes;
     private boolean visible = false;
+    private PushoverClient client;
+    private SQLiteDatabase sqLitePushHistory;
+    private DBPush dbPush;
+    private TextWatcher textWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sendpush);
 
+        btnSend = findViewById(R.id.btnSend);
+
+        dbPush = new DBPush(this, DBPush.DATABASE_NAME, null, DBPush.DATABASE_VERSION);
+        sqLitePushHistory = dbPush.getWritableDatabase();
+
+        client = new PushoverRestClient();
+
+        textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (etDK.getText().length()>0&&etNTitle.getText().length()>0&&etNT.getText().length()>0) {
+                    btnSend.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        etDK = findViewById(R.id.etDK);
+        etNTitle = findViewById(R.id.etNTitle);
+        etNT = findViewById(R.id.etNT);
         etMinutes = findViewById(R.id.etMinutes);
 
         checkBox = findViewById(R.id.checkBox);
@@ -30,6 +77,37 @@ public class SendPushActivity extends AppCompatActivity {
                 } else {
                     etMinutes.setVisibility(View.VISIBLE);
                     visible = true;
+                }
+            }
+        });
+
+        etDK.addTextChangedListener(textWatcher);
+        etNTitle.addTextChangedListener(textWatcher);
+        etNT.addTextChangedListener(textWatcher);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    client.pushMessage(PushoverMessage.builderWithApiToken("us76xvzndjtj2ryytv7j9y8fib3ghk")
+                            .setUserId(etDK.getText().toString())
+                            .setTitle(etNTitle.getText().toString())
+                            .setMessage(etNT.getText().toString())
+                            .build());
+                    if (etMinutes.getText().length()>0) {
+                        savePushToHistory(sqLitePushHistory, System.currentTimeMillis()
+                                        +(Long.valueOf(etMinutes.getText().toString())*60000),
+                                etNTitle.getText().toString(), etNT.getText().toString(),
+                                etDK.getText().toString());
+                    } else {
+                        savePushToHistory(sqLitePushHistory, System.currentTimeMillis(),
+                                etNTitle.getText().toString(), etNT.getText().toString(),
+                                etDK.getText().toString());
+                    }
+                } catch (PushoverException e) {
+                    Snackbar.make(v, "При отправке пуша произошла ошибка", BaseTransientBottomBar.LENGTH_LONG);
+                } catch (Exception e) {
+                    Snackbar.make(v, "Произошла чудовищная ошибка", BaseTransientBottomBar.LENGTH_LONG);
                 }
             }
         });
